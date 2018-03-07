d628f1505f66c9259ee3c07daa04741e21b0315d
Vladislav Senyukov <vladislav.senyukov@gmail.com>
2018-03-07 22:27:19 +0200

    Initial Commit

---
+++ LivestreamFeedViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LivestreamFeedViewer
+//
+//  Created by Vladislav Senyukov on 07.03.2018.
+//  Copyright © 2018 Vladislav Senyukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LivestreamFeedViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LivestreamFeedViewer
+//
+//  Created by Vladislav Senyukov on 07.03.2018.
+//  Copyright © 2018 Vladislav Senyukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
